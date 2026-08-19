29ff55df0cb757fd3b774aeda19f2b8219819706
Omar Alsady <omaralsady@Omars-Air.home>
2026-08-20 00:44:10 +0200

    fix stripe env key

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,6 +1,6 @@
 const Stripe = require("stripe");
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_secret_key);
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
